634add33d5c29a226966fb8224270c9dbc8f2951
redghost <94316140+R3D-GHOST@users.noreply.github.com>
2023-07-31 13:33:06 +0200

    Add files via upload

---
+++ autoawesome.sh
@@ -0,0 +1,134 @@
+clear
+echo "Iniciando script"
+sleep 1 
+clear
+#banner 
+function banner(){
+
+        echo " █████╗ ██╗   ██╗████████╗ ██████╗  █████╗ ██╗    ██╗███████╗███████╗ ██████╗ ███╗   ███╗███████╗"
+        echo "██╔══██╗██║   ██║╚══██╔══╝██╔═══██╗██╔══██╗██║    ██║██╔════╝██╔════╝██╔═══██╗████╗ ████║██╔════╝"
+        echo "███████║██║   ██║   ██║   ██║   ██║███████║██║ █╗ ██║█████╗  ███████╗██║   ██║██╔████╔██║█████╗     (R3D-GH0ST)"
+        echo "██╔══██║██║   ██║   ██║   ██║   ██║██╔══██║██║███╗██║██╔══╝  ╚════██║██║   ██║██║╚██╔╝██║██╔══╝  "
+        echo "██║  ██║╚██████╔╝   ██║   ╚██████╔╝██║  ██║╚███╔███╔╝███████╗███████║╚██████╔╝██║ ╚═╝ ██║███████╗"
+        echo "╚═╝  ╚═╝ ╚═════╝    ╚═╝    ╚═════╝ ╚═╝  ╚═╝ ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝"
+
+}
+
+function depe(){
+    
+    clear
+    # Instalacion de yay
+    sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+    cd ../
+    # yay depencias
+    yay -S picom-ibhagwan-git todo-bin alacritty rofi todo-bin acpi acpid \
+    wireless_tools jq inotify-tools polkit-gnome xdotool xclip maim \
+    brightnessctl alsa-utils alsa-tools pulseaudio lm_sensors \
+    mpd mpc mpdris2 ncmpcpp playerctl zsh-autosuggestions zsh-syntax-highlighting --needed --noconfirm
+    # kitty
+    sudo pacman -S kitty
+    # zip
+    sudo pacman -S unzip 
+    # wget
+    sudo pacman -S wget
+
+    # systemctl enable
+    systemctl enable mpd.service
+    systemctl start mpd.service
+    sudo systemctl enable acpid.service
+    sudo systemctl start acpid.service
+}
+
+function awesome(){
+
+    yay -S awesome-git --needed --noconfirm
+
+}
+
+function fonts(){
+    # fonts
+    yay -S ttf-material-design-icons --needed --noconfirm
+
+    sleep 1
+
+    sudo unzip config.zip
+
+    cp config/.Xresources $HOME
+    cp config/.profile $HOME
+    cp -r config/* "$HOME"/.config
+
+
+    sleep 3
+
+    clear
+
+    sudo unzip files/iconmoon.zip
+    cd files/
+    sudo mv icomoon/*.ttf /usr/share/fonts
+    cd ../
+    sudo cp -r Hack.zip /usr/share/fonts
+    cd /usr/share/fonts/
+    clear
+    sudo unzip Hack.zip
+    cd
+}
+
+function config(){
+
+    # Instalando p10k
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.powerlevel10k
+    echo 'source ~/.powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+    
+    # Instalando p10k root
+    sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /root/.powerlevel10k
+    
+    # kitty conf
+    sudo cp -r /kitty /opt/
+    sudo cp -r /config/kitty /root/.config/
+    # zsh
+    rm -rf ~/.zshrc
+    cp -r /.zshrc ~/.zshrc
+    
+    # zsh root
+    cp -r /.p10k.zsh ~/.p10k.zsh
+    sudo cp -r /.p10k.zsh-root /root/.p10k.zsh
+
+    # Plugins
+    sudo yay -S zsh-syntax-highlighting zsh-autosuggestions zsh-autocomplete
+
+    sudo mkdir /usr/share/zsh-sudo
+
+    cd /usr/share/zsh-sudo
+
+    sudo wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/sudo/sudo.plugin.zsh
+
+    chsh -s /usr/bin/zsh
+    sudo usermod --shell /usr/bin/zsh root
+    sudo ln -s -fv ~/.zshrc /root/.zshrc
+
+
+
+}
+
+function menu(){
+    clear
+    banner
+    sleep 1
+    echo ""
+    echo "1 --> Instalar Awesome WM"
+    sleep 1
+    echo ""
+    echo "2 --> Salir"
+    echo ""
+    read -p "--> " menu
+    
+    if [ $menu == 1 ]; then
+        depe
+        awesome
+        fonts
+        config
+fi        
+
+}
+
+menu
