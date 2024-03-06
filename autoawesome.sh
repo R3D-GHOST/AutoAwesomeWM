a3aa3c605388c51ece67e1da2f39a0e94fd5788f
redghost <94316140+R3D-GHOST@users.noreply.github.com>
2024-03-06 09:08:26 +0100

    Update autoawesome.sh

--- autoawesome.sh
+++ autoawesome.sh
@@ -21,11 +21,14 @@ function depe(){
     cd yay 
     makepkg -si
     cd ../
+    sleep 2
+    clear
     # yay depencias
     yay -Sy awesome-git picom-git alacritty rofi todo-bin acpi acpid \
     wireless_tools jq inotify-tools polkit-gnome xdotool xclip maim \
     brightnessctl alsa-utils alsa-tools pulseaudio lm_sensors \
     mpd mpc mpdris2 ncmpcpp playerctl --needed 
+
     # kitty
     sudo pacman -S kitty
     # zip
