0330df2204d4d60162f227127cd76180fa9712d4
redghost <94316140+R3D-GHOST@users.noreply.github.com>
2024-06-17 08:58:41 +0200

    Update autoawesome.sh

--- autoawesome.sh
+++ autoawesome.sh
@@ -1,65 +1,52 @@
-clear
+#!/bin/bash
+
 echo "Iniciando script"
 clear
-#banner 
-function banner(){
 
-        echo " █████╗ ██╗   ██╗████████╗ ██████╗  █████╗ ██╗    ██╗███████╗███████╗ ██████╗ ███╗   ███╗███████╗"
-        echo "██╔══██╗██║   ██║╚══██╔══╝██╔═══██╗██╔══██╗██║    ██║██╔════╝██╔════╝██╔═══██╗████╗ ████║██╔════╝"
-        echo "███████║██║   ██║   ██║   ██║   ██║███████║██║ █╗ ██║█████╗  ███████╗██║   ██║██╔████╔██║█████╗     (R3D-GH0ST)"
-        echo "██╔══██║██║   ██║   ██║   ██║   ██║██╔══██║██║███╗██║██╔══╝  ╚════██║██║   ██║██║╚██╔╝██║██╔══╝  "
-        echo "██║  ██║╚██████╔╝   ██║   ╚██████╔╝██║  ██║╚███╔███╔╝███████╗███████║╚██████╔╝██║ ╚═╝ ██║███████╗"
-        echo "╚═╝  ╚═╝ ╚═════╝    ╚═╝    ╚═════╝ ╚═╝  ╚═╝ ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝"
-
+# Function to display banner
+function banner() {
+    echo " █████╗ ██╗   ██╗████████╗ ██████╗  █████╗ ██╗    ██╗███████╗███████╗ ██████╗ ███╗   ███╗███████╗"
+    echo "██╔══██╗██║   ██║╚══██╔══╝██╔═══██╗██╔══██╗██║    ██║██╔════╝██╔════╝██╔═══██╗████╗ ████║██╔════╝"
+    echo "███████║██║   ██║   ██║   ██║   ██║███████║██║ █╗ ██║█████╗  ███████╗██║   ██║██╔████╔██║█████╗     (R3D-GH0ST)"
+    echo "██╔══██║██║   ██║   ██║   ██║   ██║██╔══██║██║███╗██║██╔══╝  ╚════██║██║   ██║██║╚██╔╝██║██╔══╝  "
+    echo "██║  ██║╚██████╔╝   ██║   ╚██████╔╝██║  ██║╚███╔███╔╝███████╗███████║╚██████╔╝██║ ╚═╝ ██║███████╗"
+    echo "╚═╝  ╚═╝ ╚═════╝    ╚═╝    ╚═════╝ ╚═╝  ╚═╝ ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝"
 }
 
-function depe(){
-    
+# Function to install dependencies
+function depe() {
     clear
-    # Instalacion de paru
+    # Install paru
     sudo pacman -S --needed base-devel git
-    git clone https://aur.archlinux.org/paru.git
+    if [ ! -d "paru" ]; then
+        git clone https://aur.archlinux.org/paru.git
+    fi
     cd paru
     makepkg -si
+    cd ..
     sleep 2
     clear
-    # paru depencias
-    paru -Sy awesome-git picom-git alacritty rofi todo-bin acpi acpid \
+
+    # Install dependencies via paru
+    paru -Sy --needed awesome-git picom-git alacritty rofi todo-bin acpi acpid \
     wireless_tools jq inotify-tools polkit-gnome xdotool xclip maim \
     brightnessctl alsa-utils alsa-tools pulseaudio lm_sensors \
-    mpd mpc mpdris2 ncmpcpp playerctl --needed 
+    mpd mpc mpdris2 ncmpcpp playerctl kitty unzip wget feh
 
-    # kitty
-    sudo pacman -S kitty
-    # zip
-    sudo pacman -S unzip 
-    # wget
-    sudo pacman -S wget
-    # feh
-    sudo pacman -S feh
-    
-
-    # systemctl enable
-    systemctl enable mpd.service
-    systemctl start mpd.service
-    sudo systemctl enable acpid.service
-    sudo systemctl start acpid.service
+    # Enable and start services
+    sudo systemctl enable --now mpd.service
+    sudo systemctl enable --now acpid.service
 }
 
-function awesome(){
-
-    paru -S awesome-git --needed --noconfirm
-
-
+# Function to install awesome-git
+function awesome() {
+    paru -S --needed --noconfirm awesome-git
 }
 
-function fonts(){
-    # fonts
-    paru -S ttf-material-design-icons --needed --noconfirm
-
-    paru -S ttf-iosevka
-
-    paru -S ttf-material-design-icons
+# Function to install fonts
+function fonts() {
+    # Install fonts
+    paru -S --needed --noconfirm ttf-material-design-icons ttf-iosevka
 
     sleep 1
 
@@ -69,63 +56,55 @@ function fonts(){
     cp config/.profile $HOME
     cp -r config/* "$HOME"/.config
 
-
     sleep 1
 
     clear
 
-    sudo unzip files/iconmoon.zip
-    cd files/
-    sudo mv icomoon/*.ttf /usr/share/fonts
-    cd ../
+    sudo unzip files/iconmoon.zip -d files/
+    sudo mv files/icomoon/*.ttf /usr/share/fonts
     sudo cp -r Hack.zip /usr/share/fonts
-    cd /usr/share/fonts/
-    clear
-    sudo unzip Hack.zip
-    cd
+    sudo unzip /usr/share/fonts/Hack.zip -d /usr/share/fonts/
+    sudo fc-cache -fv
 }
 
-function config(){
-
-    # Instalando p10k
+# Function to configure system
+function config() {
+    # Install p10k
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.powerlevel10k
     echo 'source ~/.powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
     
-    # Instalando p10k root
+    # Install p10k for root
     sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /root/.powerlevel10k
     clear
     
-    # kitty conf
-    sudo cp -r /kitty /opt/
-    sudo cp -r /config/kitty /root/.config/
-    # zsh
-    rm -rf ~/.zshrc
-    cp -r .zshrc ~/
+    # Kitty config
+    sudo cp -r config/kitty /root/.config/
+    sudo cp -r config/kitty /opt/
     
-    # zsh root
-    cp -r .p10k.zsh ~/.
-    sudo cp -r .p10k.zsh-root /root/
+    # Zsh configuration
+    cp .zshrc ~/
+    sudo cp .zshrc /root/
+    cp .p10k.zsh ~/
+    sudo cp .p10k.zsh-root /root/
 
-    # Plugins
-    paru -S zsh-syntax-highlighting zsh-autosuggestions zsh-autocomplete
+    # Install zsh plugins
+    paru -S --needed --noconfirm zsh-syntax-highlighting zsh-autosuggestions zsh-autocomplete
 
-    sudo mkdir /usr/share/zsh-sudo
+    # Install and configure sudo plugin for zsh
+    sudo mkdir -p /usr/share/zsh-sudo
+    sudo wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/sudo/sudo.plugin.zsh -O /usr/share/zsh-sudo/sudo.plugin.zsh
 
-    cd /usr/share/zsh-sudo
-
-    sudo wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/sudo/sudo.plugin.zsh
-
+    # Set zsh as the default shell
     chsh -s /usr/bin/zsh
     sudo usermod --shell /usr/bin/zsh root
     sudo ln -s -fv ~/.zshrc /root/.zshrc
-
-    
-
 }
 
+# Main script execution
 banner
 depe
 awesome
 fonts
 config
 
+echo "Script completed successfully."
